10fe7845640b5c8187bc6727ac83ede5cd320ed3
Lucio Paiva <luciopaiva@gmail.com>
2017-01-06 17:19:29 -0200

    Encapsulate index.js stuff into a class

--- index.js
+++ index.js
@@ -7,135 +7,146 @@ const
     WORLD_WIDTH = 800,
     WORLD_HEIGHT = 500;
 
-let
-    started = false,
-    showGrid = true,
-    renderMode = RENDER_MODE_NORMAL,
-    sim;
 
-function setup() {
-    const canvas = createCanvas(WORLD_WIDTH, WORLD_HEIGHT);
-    canvas.parent('canvas-container');
-    background(51);
-    fill(255, 204, 100);
-    stroke(200, 160, 50);
+class MainApp {
 
-    randomSeed(42);
-    sim = new Simulation(WORLD_WIDTH, WORLD_HEIGHT);
-}
-
-function draw() {
-    if (!started) return;
-
-    background(51);  // clear scene
-
-    if (showGrid) {
-        // ToDo is it possible to render this once? Maybe 2 separate canvases on top of each other?
-        // spatial index cell grid
-        const w = SIMULATION_CULLING_RADIUS;
-        stroke(150, 150, 255);
-        fill(150, 150, 255);
-        for (let x = w; x < WORLD_WIDTH; x += w) {
-            line(x, 0, x, WORLD_HEIGHT);
-        }
-        for (let y = w; y < WORLD_HEIGHT; y += w) {
-            line(0, y, WORLD_WIDTH, y);
-        }
-        for (let i = 0, y = 0; y < WORLD_HEIGHT; y += w) {
-            for (let x = 0; x < WORLD_WIDTH; x += w) {
-                text(i++, x + 2, y + 12);
-            }
-        }
+    constructor () {
+        this.started = false;
+        this.showGrid = true;
+        this.renderMode = RENDER_MODE_NORMAL;
     }
 
-    sim.step();
-
-    let accruedSpeeds = 0;
-
-    stroke(200, 160, 50);
-    if (renderMode == RENDER_MODE_NORMAL) {
+    setup() {
+        const canvas = createCanvas(WORLD_WIDTH, WORLD_HEIGHT);
+        canvas.parent('canvas-container');
+        background(51);
         fill(255, 204, 100);
+        stroke(200, 160, 50);
+
+        randomSeed(42);
+        this.sim = new Simulation(WORLD_WIDTH, WORLD_HEIGHT);
     }
 
-    for (const particle of sim.getParticles()) {
-        const pos = particle.getPos();
+    draw() {
+        if (!this.started) return;
 
-        if (renderMode == RENDER_MODE_HEADING) {
-            // debug heading
-            let heading = particle.getVelocity().heading() + Math.PI;  // value between -PI and PI
-            heading = Math.floor(heading / HALF_PI);
-            switch (heading) {
-                case 0: fill(255, 0, 0); break;
-                case 1: fill(0, 255, 0); break;
-                case 2: fill(0, 0, 255); break;
-                case 3: fill(255, 0, 255); break;
-                default: fill(255, 255, 255); break;
+        background(51);  // clear scene
+
+        if (this.showGrid) {
+            // ToDo is it possible to render this once? Maybe 2 separate canvases on top of each other?
+            // spatial index cell grid
+            const w = SIMULATION_CULLING_RADIUS;
+            stroke(150, 150, 255);
+            fill(150, 150, 255);
+            for (let x = w; x < WORLD_WIDTH; x += w) {
+                line(x, 0, x, WORLD_HEIGHT);
             }
-        } else if (renderMode == RENDER_MODE_VELOCITY) {
-            // debug speed
+            for (let y = w; y < WORLD_HEIGHT; y += w) {
+                line(0, y, WORLD_WIDTH, y);
+            }
+            for (let i = 0, y = 0; y < WORLD_HEIGHT; y += w) {
+                for (let x = 0; x < WORLD_WIDTH; x += w) {
+                    text(i++, x + 2, y + 12);
+                }
+            }
+        }
+
+        this.sim.step();
+
+        let accruedSpeeds = 0;
+
+        stroke(200, 160, 50);
+        if (this.renderMode == RENDER_MODE_NORMAL) {
+            fill(255, 204, 100);
+        }
+
+        for (const particle of this.sim.getParticles()) {
+            const pos = particle.getPos();
+
+            if (this.renderMode == RENDER_MODE_HEADING) {
+                // debug heading
+                let heading = particle.getVelocity().heading() + Math.PI;  // value between -PI and PI
+                heading = Math.floor(heading / HALF_PI);
+                switch (heading) {
+                    case 0: fill(255, 0, 0); break;
+                    case 1: fill(0, 255, 0); break;
+                    case 2: fill(0, 0, 255); break;
+                    case 3: fill(255, 0, 255); break;
+                    default: fill(255, 255, 255); break;
+                }
+            } else if (this.renderMode == RENDER_MODE_VELOCITY) {
+                // debug speed
+                let speed = particle.getVelocity().mag();
+                speed = Math.min(speed, SIMULATION_SPEED_LIMIT) / SIMULATION_SPEED_LIMIT;
+                speed = Math.floor(speed * 255);
+                fill(255, 204, 100, speed);
+            }
+
             let speed = particle.getVelocity().mag();
-            speed = Math.min(speed, SIMULATION_SPEED_LIMIT) / SIMULATION_SPEED_LIMIT;
-            speed = Math.floor(speed * 255);
-            fill(255, 204, 100, speed);
+            if (isNaN(speed)) {
+                console.error(particle);
+            } else {
+                accruedSpeeds += speed;
+            }
+
+            ellipse(pos.x, pos.y, 8, 8);
+            // text(particle.getIndex(), pos.x + 10, pos.y);
         }
 
-        let speed = particle.getVelocity().mag();
-        if (isNaN(speed)) {
-            console.error(particle);
-        } else {
-            accruedSpeeds += speed;
+        const selectedParticle = this.sim.getSelectedParticle();
+        if (selectedParticle) {
+            fill(0, 255, 0);
+            ellipse(selectedParticle.getPos().x, selectedParticle.getPos().y, 8, 8);
+            noFill();
+            ellipse(selectedParticle.getPos().x, selectedParticle.getPos().y,
+                SIMULATION_CULLING_DIAMETER, SIMULATION_CULLING_DIAMETER);
+            fill(255, 0, 0);
+            for (const neighbor of this.sim.getSelectedNeighbors()) {
+                ellipse(neighbor.getPos().x, neighbor.getPos().y, 8, 8);
+            }
         }
 
-        ellipse(pos.x, pos.y, 8, 8);
-        // text(particle.getIndex(), pos.x + 10, pos.y);
+        accruedSpeeds /= this.sim.getParticles().length;
+        logger.logAvgSpeed(accruedSpeeds);
+        logger.logNumParticles(this.sim.getParticles().length);
+        logger.logFps(frameRate());
     }
 
-    const selectedParticle = sim.getSelectedParticle();
-    if (selectedParticle) {
-        fill(0, 255, 0);
-        ellipse(selectedParticle.getPos().x, selectedParticle.getPos().y, 8, 8);
-        noFill();
-        ellipse(selectedParticle.getPos().x, selectedParticle.getPos().y,
-            SIMULATION_CULLING_DIAMETER, SIMULATION_CULLING_DIAMETER);
-        fill(255, 0, 0);
-        for (const neighbor of sim.getSelectedNeighbors()) {
-            ellipse(neighbor.getPos().x, neighbor.getPos().y, 8, 8);
+    keyPressed() {
+        switch (keyCode) {
+            case RETURN:
+                this.started = !this.started;
+                break;
+            case 49:  // 1
+                this.renderMode = RENDER_MODE_NORMAL;
+                break;
+            case 50:  // 2
+                this.renderMode = RENDER_MODE_HEADING;
+                break;
+            case 51:  // 3
+                this.renderMode = RENDER_MODE_VELOCITY;
+                break;
+            case 71:  // g
+                this.showGrid = !this.showGrid;
+                break;
+            case 87:  // w
+                this.sim.toggleRepellingWalls();
+                break;
+            case 75:  // k
+                this.sim.resetParticlesVelocities();
+                break;
+            case 82:  // r
+                this.sim.toggleRandomParticle();
+                break;
+            default:
+                console.info('keyCode = ' + keyCode);
         }
-    }
-
-    accruedSpeeds /= sim.getParticles().length;
-    logger.logAvgSpeed(accruedSpeeds);
-    logger.logNumParticles(sim.getParticles().length);
-    logger.logFps(frameRate());
-}
-
-function keyPressed() {
-    switch (keyCode) {
-        case RETURN:
-            started = !started;
-            break;
-        case 49:  // 1
-            renderMode = RENDER_MODE_NORMAL;
-            break;
-        case 50:  // 2
-            renderMode = RENDER_MODE_HEADING;
-            break;
-        case 51:  // 3
-            renderMode = RENDER_MODE_VELOCITY;
-            break;
-        case 71:  // g
-            showGrid = !showGrid;
-            break;
-        case 87:  // w
-            sim.toggleRepellingWalls();
-            break;
-        case 75:  // k
-            sim.resetParticlesVelocities();
-            break;
-        case 82:  // r
-            sim.toggleRandomParticle();
-            break;
-        default:
-            console.info('keyCode = ' + keyCode);
     }
 }
+
+const mainApp = new MainApp();
+
+// p5.js callbacks
+function setup() { mainApp.setup(); }
+function draw() { mainApp.draw(); }
+function keyPressed() { mainApp.keyPressed(); }
